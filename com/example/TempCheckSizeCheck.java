9cea8439db5cb9a17cd4fa9242c0abc7d60f0d5e
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-03-22 00:11:48 +0000

    Restart fixes

--- com/example/TempCheckSizeCheck.java
+++ com/example/TempCheckSizeCheck.java
@@ -40,126 +40,152 @@ public final class TempCheckSizeCheck {
         return TriAreaFLT;
     }
 
+    public static float shapeTrapezoid(float TrapBaseAFlt, float TrapBaseBFlt, float TampHeightFlt) {
+        Float TrapAreaFLT = (float) ((TrapBaseAFlt + TrapBaseBFlt) / 2) * 5;
+        return TrapAreaFLT;
+    }
+
+    public static float shapePentagon(float sideLengthFLT) {
+        Float PentAreaFLT = (float) (0.25 * Math.sqrt(5 * (5 + 2 * Math.sqrt(5))) * Math.pow(sideLengthFLT, 2));
+        return PentAreaFLT;
+    }
+
     public static void main(final String[] args) {
         final Scanner in = new Scanner(System.in);
         boolean programState = true;
         while (programState) {
-            System.out.println("Enter 1 to start or 0 to quit");
-            String programStateSTR = in.nextLine();
-            try {
-                Integer programStateINT = Integer.parseInt(programStateSTR);
-                if (programStateINT == 1) {
-                    System.out.println("Which program would you like to run? 1 = Temp calculator. 2 = Shape area calculator");
-                    final String programPickSTR = in.nextLine();
-                    try {
-                        Integer programPickINT = Integer.parseInt(programPickSTR);
-                        switch (programPickINT) {
-                            case 1:
-                                System.out.println("Program 1 play");
-                                System.out.print("Welcome to my program, enter the temperature in Celsius");
-                                System.out.println(" and it'll be converted into Kelvin and Fahrenheit!");
-                                final String CelsiusSTR = in.nextLine();
-                                try {
-                                    Float CelsiusFLT = Float.parseFloat(CelsiusSTR);
-                                    Float KelvinFLT = convertTempKel(CelsiusFLT);
-                                    Float FahrenheitFLT = convertTempFah(CelsiusFLT);
-                                    System.out.println(KelvinFLT + " " + FahrenheitFLT);
-                                } catch (NumberFormatException error) {
-                                    System.out.println("Bad input");
-                                }
-                                break;
-                            case 2:
-                                System.out.println("Program 2 play");
-                                System.out.println("Welcome to my shape area calculator");
-                                System.out.println("Enter which shape you would use to calculate");
-                                System.out.println("1 = Triangle, 2 = trapezoid, 3 = pentagon");
-                                final String shapePickSTR = in.nextLine();
-                                try {
-                                    final Integer shapePickInt = Integer.parseInt(shapePickSTR);
-                                    switch (shapePickInt) {
-                                        case 1:
-                                            System.out.println("Enter the base of triangle");
-                                            final String TriBaseSTR = in.nextLine();
-                                            try {
-                                                final Float TriBaseFLT = Float.parseFloat(TriBaseSTR);
-                                                if (TriBaseFLT > 0) {
-                                                    System.out.println("Enter the height of the triangle");
-                                                    final String TriHeightSTR = in.nextLine();
-                                                    try {
-                                                        final Float TriHeightFLT = Float.parseFloat(TriHeightSTR);
-                                                        if (TriHeightFLT > 0) {
-                                                            //TriAreaFLT
-                                                            Float TriAreaFLT = shapeTriangle(TriBaseFLT, TriHeightFLT);
-                                                            System.out.println(TriAreaFLT);
-                                                        }
-                                                    } catch (NumberFormatException Error) {
-                                                        System.out.println("Bad input base");
-                                                        break;
+            System.out.println("Enter Y to start, anything else to quit");
+            String game = in.nextLine();
+            if (game.equalsIgnoreCase("y")) {
+                System.out.println("Which program would you like to run? 1 = Temp calculator. 2 = Shape area calculator");
+                final String programPickSTR = in.nextLine();
+                try {
+                    Integer programPickINT = Integer.parseInt(programPickSTR);
+                    switch (programPickINT) {
+                        case 1:
+                            System.out.println("Program 1 play");
+                            System.out.print("Welcome to my program, enter the temperature in Celsius");
+                            System.out.println(" and it'll be converted into Kelvin and Fahrenheit!");
+                            final String CelsiusSTR = in.nextLine();
+                            try {
+                                Float CelsiusFLT = Float.parseFloat(CelsiusSTR);
+                                Float KelvinFLT = convertTempKel(CelsiusFLT);
+                                Float FahrenheitFLT = convertTempFah(CelsiusFLT);
+                                System.out.println(KelvinFLT + " " + FahrenheitFLT);
+                            } catch (NumberFormatException error) {
+                                System.out.println("Bad input");
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Program 2 play");
+                            System.out.println("Welcome to my shape area calculator");
+                            System.out.println("Enter which shape you would use to calculate");
+                            System.out.println("1 = Triangle, 2 = trapezoid, 3 = pentagon");
+                            final String shapePickSTR = in.nextLine();
+                            try {
+                                final Integer shapePickInt = Integer.parseInt(shapePickSTR);
+                                switch (shapePickInt) {
+                                    case 1:
+                                        System.out.println("Enter the base of triangle");
+                                        final String TriBaseSTR = in.nextLine();
+                                        try {
+                                            final Float TriBaseFLT = Float.parseFloat(TriBaseSTR);
+                                            if (TriBaseFLT > 0) {
+                                                System.out.println("Enter the height of the triangle");
+                                                final String TriHeightSTR = in.nextLine();
+                                                try {
+                                                    final Float TriHeightFLT = Float.parseFloat(TriHeightSTR);
+                                                    if (TriHeightFLT > 0) {
+                                                        //TriAreaFLT
+                                                        Float TriAreaFLT = shapeTriangle(TriBaseFLT, TriHeightFLT);
+                                                        System.out.println(TriAreaFLT);
                                                     }
-                                                } else {
-                                                    System.out.println("Bad input");
+                                                } catch (NumberFormatException Error) {
+                                                    System.out.println("Bad input base");
+                                                    break;
                                                 }
-                                            } catch (NumberFormatException Error) {
-                                                System.out.println("Bad input case 1 try catch");
+                                            } else {
+                                                System.out.println("Bad input");
                                             }
-                                            break;
-                                        case 2:
-                                            System.out.println("Enter base A of trapezoid");
-                                            final String TrapBaseASTR = in.nextLine();
-                                            try {
-                                                final Float TrapBaseAFT = Float.parseFloat(TrapBaseASTR);
-                                                if (TrapBaseAFT > 0) {
-                                                    System.out.println("Enter the height of the triangle");
-                                                    final String TampBaseBST = in.nextLine();
-                                                    try {
-                                                        final Float TampBaseBFT = Float.parseFloat(TampBaseBST);
-                                                        if (TampBaseBFT > 0) {
-                                                            System.out.println("Enter the height of the tamp");
-                                                            final String TampHeightSTR = in.nextLine();
-                                                            try {
-                                                                final Float TampHeightFLT = Float.parseFloat(TampHeightSTR);
-                                                            } catch (NumberFormatException Error) {
-                                                                System.out.println("Bad input height flt");
+                                        } catch (NumberFormatException Error) {
+                                            System.out.println("Bad input case 1 try catch");
+                                        }
+                                        break;
+                                    case 2:
+                                        System.out.println("Enter base A of the trapezoid");
+                                        final String TrapBaseAStr = in.nextLine();
+                                        try {
+                                            final Float TrapBaseAFlt = Float.parseFloat(TrapBaseAStr);
+                                            if (TrapBaseAFlt > 0) {
+                                                System.out.println("Enter base B of the trapezoid");
+                                                final String TrapBaseBStr = in.nextLine();
+                                                try {
+                                                    final Float TrapBaseBFlt = Float.parseFloat(TrapBaseBStr);
+                                                    if (TrapBaseBFlt > 0) {
+                                                        System.out.println("Enter the height of the trapezoid");
+                                                        final String TampHeightStr = in.nextLine();
+                                                        try {
+                                                            final Float TampHeightFlt = Float.parseFloat(TampHeightStr);
+                                                            if (TampHeightFlt > 0) {
+                                                                Float TrapAreaFLT = shapeTrapezoid(TrapBaseAFlt,TrapBaseBFlt, TampHeightFlt);
+                                                                System.out.println(TrapAreaFLT);
+                                                            } else {
+                                                                System.out.println("Invalid height inputted!");
                                                             }
-                                                        } else {
-                                                            System.out.println("Bad input base B");
+                                                            
+                                                        } catch (NumberFormatException Error) {
+                                                            System.out.println("Bad input height flt");
                                                         }
-                                                    } catch (NumberFormatException Error) {
-                                                        System.out.println("Bad input base A");
-                                                        break;
+                                                    } else {
+                                                        System.out.println("Bad input base B");
                                                     }
-                                                } else {
-                                                    System.out.println("Bad input");
+                                                } catch (NumberFormatException Error) {
+                                                    System.out.println("Bad input base A");
+                                                    break;
                                                 }
-                                            } catch (NumberFormatException Error) {
-                                                System.out.println("Bad input Base A");
+                                            } else {
+                                                System.out.println("Bad input");
                                             }
-                                            break;
-                                        case 3:
-                                            System.out.println("test 3");
-                                            break;
-                                        default:
-                                            System.out.println("Bad case");
-                                            break;
-                                    }
-                                } catch(NumberFormatException Error) {
-
+                                        } catch (NumberFormatException Error) {
+                                            System.out.println("Bad input Base A");
+                                        }
+                                        break;
+                                    case 3:
+                                        System.out.println("Enter the side length");
+                                        final String sideLengthSTR = in.nextLine();
+                                        try {
+                                            final float sideLengthFLT = Float.parseFloat(sideLengthSTR);
+                                            if (sideLengthFLT > 0) {
+                                                Float PentAreaFLT = shapePentagon(sideLengthFLT);
+                                                System.out.println(PentAreaFLT);
+                                            }
+                                        } catch (NumberFormatException Error) {
+                                            System.out.println("Bad input side length");
+                                        }
+                                        break;
+                                    default:
+                                        System.out.println("Bad case");
+                                        break;
                                 }
+                            } catch(NumberFormatException Error) {
+                                System.out.println("Bad shape picker");
+                            }
 
-                                break;
-                            default:
-                                System.out.println("Bad input case, ending program.");
-                                break;
-                        }
-                    } catch (NumberFormatException Error) {
-                        System.out.println("Invalid input, restarting start");
+                            break;
+                        default:
+                            System.out.println("Bad input case, ending program.");
+                            break;
                     }
-                    break;
-                } else {
-                    break;
+                } catch (NumberFormatException Error) {
+                    System.out.println("Invalid program option!");
                 }
-            } catch (NumberFormatException error) {
-                System.out.println("Bad input, try again.");
+                System.out.println("Enter anything to restart or Q to quit");
+                String restart = in.nextLine();
+                if (restart.equalsIgnoreCase("q")) {
+                    programState = false;
+                }
+            } else {
+                break;
             }
         }
     }
